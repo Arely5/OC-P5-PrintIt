62c7331489af8fff8f4813f5e72f44c8fb2de88b
Arely <valentinaadelaide@gmail.com>
2024-01-10 23:14:57 +0100

    JS: défilement infini carrousel

--- assets/script.js
+++ assets/script.js
@@ -1,43 +1,68 @@
-// Ajout d'event listenner sur les flèches
-var arrowLeft = document.getElementById('arrow_left');
-var arrowRight = document.getElementById('arrow_right');
+// Gestion des slides
 
-arrowLeft.addEventListener('click', showMsg);
-arrowRight.addEventListener('click', showAutreMsg);
-
-function showMsg() {
-	alert("it works :)");
-}
-
-function showAutreMsg() {
-	alert("it works too :3");
-}
-// Fin de l'ajout d'event listenner
-
-// Gestion des slides et bullets points
+// Event listener
+const arrowLeft = document.getElementById('arrow_left');
+const arrowRight = document.getElementById('arrow_right');
 const slides = [
 	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
+		image: "./assets/images/slideshow/slide1.jpg",
+		texte:"Impressions tous formats <span>en boutique et en ligne</span>"
 	},
 	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+		image:"./assets/images/slideshow/slide2.jpg",
+		texte:"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
 	},
 	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
+		image:"./assets/images/slideshow/slide3.jpg",
+		texte:"Grand choix de couleurs <span>de CMJN aux pantones</span>"
 	},
 	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
+		image:"./assets/images/slideshow/slide4.png",
+		texte:"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+let slideIndex = 0;
+
+arrowLeft.addEventListener('click', previousSlide);
+arrowRight.addEventListener('click', nextSlide);
+
+// Changement de slides
+function previousSlide() {
+	slideIndex--;
+	dotsIndex--;
+
+	if (slideIndex < 0) {
+		slideIndex = slides.length -1;
+	}
+
+	showCurrentSlide();
+}
+
+function nextSlide() {
+	slideIndex++;
+	dotsIndex++;
+
+	if (slideIndex >= slides.length) {
+		slideIndex = 0;
+	}
+
+	showCurrentSlide();
+}
+
+function showCurrentSlide() {
+	const imageElement = document.querySelector(".banner-img");
+	const textElement = document.querySelector(".banner-title");
+
+	imageElement.src = slides[slideIndex].image;
+	textElement.innerHTML = slides[slideIndex].texte;
+}
+//fin gestion des slides
 
 // Bullet points
 var dotsNumber = slides.length;
 
 var dots = document.getElementById('dots');
+let dotsIndex = 0;
 
 for (var i = 0; i < slides.length; i++) {
 	var dot = document.createElement('div');
